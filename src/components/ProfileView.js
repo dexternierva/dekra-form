44a7c59a414b9bc1b3a23cc5a759fd163ae79a6d
dexternierva <dexternierva@gmail.com>
2021-08-05 08:38:57 +0800

    Add if condition on ProfileView for empty profile forms

--- src/components/ProfileView.js
+++ src/components/ProfileView.js
@@ -11,6 +11,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableRow from '@material-ui/core/TableRow';
+import Box from '@material-ui/core/Box';
 
 const Avatar = styled.img`
     display: block;
@@ -76,105 +77,113 @@ function  ProfileView({ response }) {
     let t = useTranslate("Profile");
     const classes = useStyles();
 
-    function createKeyFiguresData(name, value) {
-        return { name, value };
+    if (response.cv !== null) {
+        function createKeyFiguresData(name, value) {
+            return { name, value };
+        }
+        
+        const keyfigurerows = [
+            createKeyFiguresData(t('Conclusion'), response.cv.kfDegree),
+            createKeyFiguresData(t('Language skills'), response.cv.kfLanguage),
+            createKeyFiguresData(t('Experience'), response.cv.kfExperienceYear + " years(s) and " + response.cv.kfExperienceMonth + " month(s)"),
+            createKeyFiguresData(t('Priorities'), response.cv.kfPriorities),
+        ];
+
+        function personalBackgroundData(name, value) {
+            return { name, value };
+        }
+        
+        const personalbackgroundrows = [
+            personalBackgroundData(t('Address'), response.cv.address),
+            personalBackgroundData(t('E-mail Address'), response.cv.email),
+            personalBackgroundData(t('Nationality'), response.cv.nationality),
+            personalBackgroundData(t('Sex'), response.cv.sex),
+            personalBackgroundData(t('Date of birth'), response.cv.dateOfBirth),
+            personalBackgroundData(t('Place of birth'), response.cv.placeOfBirth),
+            personalBackgroundData(t('Marital status'), response.cv.maritalStatus),
+        ];
+
+        return (
+            <>
+            <Grid container spacing={3} className={classes.grid}>
+                <Grid item xs={12} sm={2}>
+                    <Avatar 
+                        component="div"
+                        src={user.avatar}
+                        className={classes.large}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={10}>
+                    <TableContainer className={classes.ftable}>
+                        <Table aria-label="simple table">
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell component="th" scope="row" align="center" className={classes.tcell}><Header>Participant ID</Header></TableCell>
+                                    <TableCell component="th" scope="row" align="center"><Header>Category</Header></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell align="center" className={classes.tcell}>{response.cv.participantId}</TableCell>
+                                    <TableCell align="center">{response.cv.category}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                    <Name>
+                        <Typography className={classes.typo} variant="h4" component="h2">{response.firstname} {response.lastname}</Typography>
+                    </Name>
+                </Grid>
+            </Grid>
+
+            <Grid container spacing={0} className={classes.grid}>
+                <Grid item xs={12} sm={3}>
+                    <SectionTitle>{t("At a glance")}</SectionTitle>
+                </Grid>
+                <Grid item xs={12} sm={9}>
+                    <TableContainer className={classes.table}>
+                        <Table aria-label="simple table">
+                            <TableBody>
+                            {keyfigurerows.map((row) => (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">{row.name}</TableCell>
+                                    <TableCell align="right">{row.value}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                            <caption>{t("Detailed overview of activities in the 'Professional Skills' facility")}</caption>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+
+            <Grid container spacing={0} className={classes.grid}>
+                <Grid item xs={12} sm={3}>
+                    <SectionTitle>{t("Private Background")}</SectionTitle>
+                </Grid>
+                <Grid item xs={12} sm={9}>
+                    <TableContainer className={classes.table}>
+                        <Table aria-label="simple table">
+                            <TableBody>
+                            {personalbackgroundrows.map((row) => (
+                                <TableRow key={row.name}>
+                                    <TableCell component="th" scope="row">{row.name}</TableCell>
+                                    <TableCell align="right">{row.value}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+            </>
+        )
+    } else {
+        return (
+            <Box p={4}>
+                <div className={classes.notification} color="secondary">Applicant has not filled out his/her Other Skills form.</div>
+            </Box>
+        )
     }
-    
-    const keyfigurerows = [
-        createKeyFiguresData(t('Conclusion'), response.cv.kfDegree),
-        createKeyFiguresData(t('Language skills'), response.cv.kfLanguage),
-        createKeyFiguresData(t('Experience'), response.cv.kfExperienceYear + " years(s) and " + response.cv.kfExperienceMonth + " month(s)"),
-        createKeyFiguresData(t('Priorities'), response.cv.kfPriorities),
-    ];
-
-    function personalBackgroundData(name, value) {
-        return { name, value };
-    }
-    
-    const personalbackgroundrows = [
-        personalBackgroundData(t('Address'), response.cv.address),
-        personalBackgroundData(t('E-mail Address'), response.cv.email),
-        personalBackgroundData(t('Nationality'), response.cv.nationality),
-        personalBackgroundData(t('Sex'), response.cv.sex),
-        personalBackgroundData(t('Date of birth'), response.cv.dateOfBirth),
-        personalBackgroundData(t('Place of birth'), response.cv.placeOfBirth),
-        personalBackgroundData(t('Marital status'), response.cv.maritalStatus),
-    ];
-
-    return (
-        <>
-        <Grid container spacing={3} className={classes.grid}>
-            <Grid item xs={12} sm={2}>
-                <Avatar 
-                    component="div"
-                    src={user.avatar}
-                    className={classes.large}
-                />
-            </Grid>
-            <Grid item xs={12} sm={10}>
-                <TableContainer className={classes.ftable}>
-                    <Table aria-label="simple table">
-                        <TableBody>
-                            <TableRow>
-                                <TableCell component="th" scope="row" align="center" className={classes.tcell}><Header>Participant ID</Header></TableCell>
-                                <TableCell component="th" scope="row" align="center"><Header>Category</Header></TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell align="center" className={classes.tcell}>{response.cv.participantId}</TableCell>
-                                <TableCell align="center">{response.cv.category}</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-
-                <Name>
-                    <Typography className={classes.typo} variant="h4" component="h2">{response.firstname} {response.lastname}</Typography>
-                </Name>
-            </Grid>
-        </Grid>
-
-        <Grid container spacing={0} className={classes.grid}>
-            <Grid item xs={12} sm={3}>
-                <SectionTitle>{t("At a glance")}</SectionTitle>
-            </Grid>
-            <Grid item xs={12} sm={9}>
-                <TableContainer className={classes.table}>
-                    <Table aria-label="simple table">
-                        <TableBody>
-                        {keyfigurerows.map((row) => (
-                            <TableRow key={row.name}>
-                                <TableCell component="th" scope="row">{row.name}</TableCell>
-                                <TableCell align="right">{row.value}</TableCell>
-                            </TableRow>
-                        ))}
-                        </TableBody>
-                        <caption>{t("Detailed overview of activities in the 'Professional Skills' facility")}</caption>
-                    </Table>
-                </TableContainer>
-            </Grid>
-        </Grid>
-
-        <Grid container spacing={0} className={classes.grid}>
-            <Grid item xs={12} sm={3}>
-                <SectionTitle>{t("Private Background")}</SectionTitle>
-            </Grid>
-            <Grid item xs={12} sm={9}>
-                <TableContainer className={classes.table}>
-                    <Table aria-label="simple table">
-                        <TableBody>
-                        {personalbackgroundrows.map((row) => (
-                            <TableRow key={row.name}>
-                                <TableCell component="th" scope="row">{row.name}</TableCell>
-                                <TableCell align="right">{row.value}</TableCell>
-                            </TableRow>
-                        ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Grid>
-        </Grid>
-        </>
-    )
 }
 
 export default ProfileView;
