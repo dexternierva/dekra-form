539241b3203613decfd8e546180bc7d81ce677cf
dexternierva <dexternierva@gmail.com>
2021-09-03 20:09:27 +0800

    The Boom

--- src/components/PracticalSkillsView.js
+++ src/components/PracticalSkillsView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Rating from '@mui/core/Rating';
+import { Rating } from '@mui/core';
 //import Rating from '@material-ui/lab/Rating';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
