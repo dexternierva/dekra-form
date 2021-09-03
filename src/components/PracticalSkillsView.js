0dd1954b83fd4172a2994536efc5aacc9fac5084
dexternierva <dexternierva@gmail.com>
2021-09-03 14:10:17 +0800

    Reinstalled the Latest MaterialUI version

--- src/components/PracticalSkillsView.js
+++ src/components/PracticalSkillsView.js
@@ -948,7 +948,6 @@ function  PracticalSkillsView({ response }) {
                                         <Rating
                                             name="highlight-selected-only"
                                             defaultValue={0}
-                                            // getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
                                             precision={1}
                                             value={response.practical_activity.a2}
                                             icon={<CheckBoxOutlinedIcon fontSize="inherit" />}
