7341a62c0e322b750d57ec7a2f115cfea6a61243
dexternierva <dexternierva@gmail.com>
2021-09-03 19:20:10 +0800

    Clean npm cache

--- src/components/PracticalSkillsView.js
+++ src/components/PracticalSkillsView.js
@@ -966,7 +966,6 @@ function  PracticalSkillsView({ response }) {
                                         <Rating
                                             name="highlight-selected-only"
                                             defaultValue={0}
-                                            // getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
                                             precision={1}
                                             value={response.practical_activity.a3}
                                             icon={<CheckBoxOutlinedIcon fontSize="inherit" />}
